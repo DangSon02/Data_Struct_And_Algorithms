64e59a3c02492876c2b6e5998e09ceb64ece7953
Dang Van Son <dangson130402@gmail.com>
2024-01-13 14:19:55 +0700

    fix insertion sort

--- MasterClass/InsertionSort/main.js
+++ MasterClass/InsertionSort/main.js
@@ -1,8 +1,8 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let currentVal = arr[i];
-    const swap = (arr, index1, index2) => {
-      return ([arr[index1 + 1], arr[index1]] = [arr[index1], currentVal]);
+    const swap = (arr, index, currentVal) => {
+      return ([arr[index + 1], arr[index]] = [arr[index], currentVal]);
     };
     for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       //   arr[j + 1] = arr[j];
